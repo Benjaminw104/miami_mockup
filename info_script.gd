01dfde6696b368e61e8751312f87f98bf3606820
Benjaminw104 <167047969+Benjaminw104@users.noreply.github.com>
2024-07-28 22:21:33 +1000

    Project Files

---
+++ info_script.gd
@@ -0,0 +1,10 @@
+class_name dev_bulletin
+extends Node
+
+func to_do_tasks():
+	#Date 28-07
+	#Change doors so enemies can only be knocked down when the door is moving. 
+	#Add 'is_moving' boolean to the door physics process. 
+	
+	
+	pass
